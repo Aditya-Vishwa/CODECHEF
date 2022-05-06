e8560bdfd34c2986cae244a6f8f475465e8be213
Aditya Kumar Sharma <vishwaaditya2003@gmail.com>
2022-05-07 00:26:13 +0530

    Sugarcane Juice Business

---
+++ SUGARCANE.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+    cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	    int a;
+	    cin>>a;
+	    int p=a*15;
+	    cout<<p<<endl;
+	}
+	return 0;
+}
